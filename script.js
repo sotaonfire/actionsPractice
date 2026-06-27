e09e12c37ec62fbb4e3d49dbeeb5df839fe59dc1
Sota <sota011002@gmail.com>
2026-06-27 20:14:01 +0900

    テキストの切り替えをvue化

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
-const buttonClass = document.querySelector(".button");
-const button_text = document.querySelector(".button_text");
-function clickButton() {
-  buttonClass.addEventListener("click", () => {
-    button_text.textContent = "ボタンが押されたよ";
-    return;
-  });
-}
-clickButton();
+const { createApp, ref } = Vue;
+
+createApp({
+  setup() {
+    const buttonText = ref("ボタンはまだ押されていないよ");
+    const clicked = ref(false);
+    const clickButton = () => {
+      if (clicked.value) {
+        clicked.value = false;
+        buttonText.value = "ボタンはまだ押されていないよ";
+      } else {
+        clicked.value = true;
+        buttonText.value = "ボタンは押されたよ";
+      }
+    };
+    return {
+      buttonText,
+      clickButton,
+    };
+  },
+}).mount("#app");
